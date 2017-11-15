9813ff974ecc69f70ce3eaf27779e9bc492926ff
WallofYawn <indiekills@gmail.com>
2017-11-15 11:59:14 -0800

    started the app structure for the upload function

--- basic-example/mysite/settings.py
+++ basic-example/mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'mysite.core',
+    'mysite.upload'
 ]
 
 MIDDLEWARE = [
--- basic-example/mysite/urls.py
+++ basic-example/mysite/urls.py
@@ -2,6 +2,11 @@ from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
 
 from mysite.core import views as core_views
+"""
+from mysite.upload import views as upload_views
+maybe? and then...
+url(r'^upload/$', upload_views.upload, name='upload'),
+"""
 
 
 urlpatterns = [
@@ -9,4 +14,5 @@ urlpatterns = [
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
     url(r'^signup/$', core_views.signup, name='signup'),
+    
 ]
